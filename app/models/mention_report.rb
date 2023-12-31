781a2c346e1c139dd03157c83ac0ad6409c01062
yocchan-git <yocchan.business@gmail.com>
2023-12-31 11:02:43 +0900

    モデルにバリデーションを追加する

--- app/models/mention_report.rb
+++ app/models/mention_report.rb
@@ -3,4 +3,7 @@
 class MentionReport < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
+
+  validates :mentioning_id, presence: true, uniqueness: { scope: :mentioned_id }
+  validates :mentioned_id, presence: true, uniqueness: { scope: :mentioning_id }
 end
