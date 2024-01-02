ee3e0048339e96c0cf0af091ad6482f3485c1fea
yocchan-git <yocchan.business@gmail.com>
2024-01-02 10:40:31 +0900

    ログアウト後のパスもapplication_controller.rbに記述

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::SessionsController < Devise::SessionsController
-  protected
-
-  def after_sign_out_path_for(_resource)
-    new_user_session_path
-  end
-end
