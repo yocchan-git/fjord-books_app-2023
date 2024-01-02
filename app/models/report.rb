ee9cf1b503ef0564c0c39899e80eea8c839d0d04
yocchan-git <yocchan.business@gmail.com>
2024-01-02 15:01:36 +0900

    belongs_toを先に記述する

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
+  has_many :comments, as: :commentable, dependent: :destroy
 end
