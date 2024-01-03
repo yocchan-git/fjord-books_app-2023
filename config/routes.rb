16994756518f14648784fdbb2729b031c45c6700
yocchan-git <yocchan.business@gmail.com>
2024-01-03 13:54:09 +0900

    ルーティングを変更する

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :reports do
-    resource :comments, only: %i(create destroy)
-  end
-  # post 'comments', to: "comments#create", as: "comments"
-  # delete 'comments/:id', to: "comments#destroy", as: "comment"
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resource :comments, only: %i(create destroy)
+    resources :comments, only: :create, module: :books
+  end
+  resources :reports do
+    resources :comments, only: :create, module: :reports
   end
   resources :users, only: %i(index show)
+  resources :comments, only: :destroy
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
