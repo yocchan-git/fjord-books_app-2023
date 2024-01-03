250645fc474197835c84d1f548c307764fce7525
yocchan-git <yocchan.business@gmail.com>
2024-01-03 13:55:16 +0900

    モデルファイルの変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
+
+  def create_user_name_or_email
+    unless user.name.empty?
+      user.name
+    else
+      user.email
+    end
+  end
 end
