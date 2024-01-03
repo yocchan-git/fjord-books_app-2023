dad2e5d6cc7e81fc0e5dba7db15a4ea4bcb4a1af
yocchan-git <yocchan.business@gmail.com>
2024-01-03 20:44:23 +0900

    モデルテストを作成する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "名前がないユーザはメールアドレスを返す" do
+    yocchan = build(:yocchan)
+    assert_equal yocchan.email, yocchan.name_or_email
+  end
+
+  test "名前があるユーザは名前を返す" do
+    takeru = build(:takeru)
+    assert_equal 'たける', takeru.name_or_email
+  end
 end
