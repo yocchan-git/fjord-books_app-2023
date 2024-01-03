9c4a4f03fbf80ce37f57e5d3dc9f8cf922a23138
yocchan-git <yocchan.business@gmail.com>
2024-01-03 10:50:05 +0900

    ユーザーidをnull禁止にする

---
+++ db/migrate/20240103014841_change_user_to_comments.rb
@@ -0,0 +1,5 @@
+class ChangeUserToComments < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :comments, :user_id, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_040551) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_014841) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_040551) do
     t.text "content"
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_040551) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
