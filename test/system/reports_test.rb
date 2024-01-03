416a0df46a21b960cb4dffd0e99659a213c6113a
yocchan-git <yocchan.business@gmail.com>
2024-01-03 20:44:33 +0900

    システムテストを作成する

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,44 +4,40 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:one)
+    @report = create(:report)
+
+    visit new_user_session_path
+    fill_in 'Eメール', with: @report.user.email
+    fill_in 'パスワード', with: @report.user.password
+    click_button 'ログイン'
   end
 
-  test 'visiting the index' do
+  test '日報の新規作成をする' do
     visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    click_on '日報の新規作成'
+
+    fill_in 'タイトル', with: @report.content
+    fill_in '内容', with: @report.title
+    click_button '登録する'
+
+    assert_text '日報が作成されました。'
   end
 
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
-
-    assert_text 'Report was successfully created'
-    click_on 'Back'
-  end
-
-  test 'should update Report' do
+  test '日報の更新をする' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on 'この日報を編集'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in 'タイトル', with: @report.content
+    fill_in '内容', with: @report.title
+    click_button '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました。'
   end
 
-  test 'should destroy Report' do
+  test '日報を削除する' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_button 'この日報を削除'
 
-    assert_text 'Report was successfully destroyed'
+    assert_text '日報が削除されました。'
   end
 end
