2fb228b7be3d0b706cab84bb092da02bbacb91aa
yocchan-git <yocchan.business@gmail.com>
2024-01-03 15:26:09 +0900

    コメントを削除した時の文言をi18nで生成する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,6 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.find(params[:id])
 
     @comment.destroy!
-    redirect_to @comment.commentable, notice: 'コメントを削除しました'
+    redirect_to @comment.commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 end
