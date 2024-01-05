cd43e18dc9e927d716d2951adb6a49f62c04160c
yocchan-git <yocchan.business@gmail.com>
2024-01-05 17:05:06 +0900

    モデルの場所と命名を変更する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,4 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 400 }
-
-  def create_user_name_or_email
-    user.name.presence || user.email
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
+
+  def name_or_email
+    name.presence || email
+  end
 end
