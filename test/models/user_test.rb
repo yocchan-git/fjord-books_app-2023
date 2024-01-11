032db4ca14b3e511d723a8978d1eaf808bf18ba2
yocchan-git <yocchan.business@gmail.com>
2024-01-11 13:56:30 +0900

    モデルテストのassertの条件を変更する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test '名前がないユーザはメールアドレスを返す' do
     yocchan = build(:user, email: 'yocchan@example.com', name: '')
-    assert_equal yocchan.email, yocchan.name_or_email
+    assert_equal 'yocchan@example.com', yocchan.name_or_email
   end
 
   test '名前があるユーザは名前を返す' do
