b9cbdd169b32576fe688aaa734bc8d1e53dff753
yocchan-git <yocchan.business@gmail.com>
2024-01-11 13:33:56 +0900

    システムテストを変更する

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,11 +4,12 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = create(:report)
+    @yocchan = create(:user, email: 'yocchan@example.com')
+    @report = create(:report, user: @yocchan)
 
     visit new_user_session_path
-    fill_in 'Eメール', with: @report.user.email
-    fill_in 'パスワード', with: @report.user.password
+    fill_in 'Eメール', with: 'yocchan@example.com'
+    fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
   end
 
@@ -16,27 +17,40 @@ class ReportsTest < ApplicationSystemTestCase
     visit reports_url
     click_on '日報の新規作成'
 
-    fill_in 'タイトル', with: @report.content
-    fill_in '内容', with: @report.title
+    fill_in 'タイトル', with: @report.title
+    fill_in '内容', with: @report.content
     click_button '登録する'
 
     assert_text '日報が作成されました。'
+
+    assert_text @report.title
+    assert_text @report.content
   end
 
   test '日報の更新をする' do
     visit report_url(@report)
     click_on 'この日報を編集'
 
-    fill_in 'タイトル', with: @report.content
-    fill_in '内容', with: @report.title
+    fill_in 'タイトル', with: @report.title + 'タイトルを更新'
+    fill_in '内容', with: @report.content + '内容を更新'
     click_button '更新する'
 
     assert_text '日報が更新されました。'
+
+    assert_text @report.title + 'タイトルを更新'
+    assert_text @report.content + '内容を更新'
   end
 
   test '日報を削除する' do
     visit report_url(@report)
+
+    assert_text @report.title
+    assert_text @report.content
+
     click_button 'この日報を削除'
+
+    assert_no_text @report.title
+    assert_no_text @report.content
 
     assert_text '日報が削除されました。'
   end
